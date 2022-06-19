65ea67f2a5c49b8f31de166f5c8f38611c3a4583
Facundo Zerbato <facundozerbato@outlook.com>
2022-06-19 19:13:39 -0300

    new project

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,8 +1,18 @@
 import img from '../../assets/github.png';
+import travel from '../../assets/travel.png';
 
 export const ProjectData = [
     {
         id: 1,
+        title: "Travel App",
+        about: "Single Page Application",
+        tags: ["ReactJs"],
+        demo: "https://travel-company.vercel.app/",
+        github: "https://github.com/FacundoZto/TravelProject",
+        image: travel
+    },
+    {
+        id: 2,
         title: "Dogs App",
         about: "Single Page Application",
         tags: ["ReactJs", "Redux", "NodeJs"],
@@ -11,7 +21,7 @@ export const ProjectData = [
         image: "https://raw.githubusercontent.com/FacundoZto/Proyecto-Dogs/main/Imagenes/Landing.png"
     },
     {
-        id: 2,
+        id: 3,
         title: "React CRUD",
         about: "Create, Read, Update, Delete",
         tags: ["ReactJs", "Redux", "NodeJs"],
@@ -20,7 +30,7 @@ export const ProjectData = [
         image: "https://github.com/FacundoZto/React-CRUD/blob/main/img/React-crud-home.png?raw=true"
     },
     {
-        id: 3,
+        id: 4,
         title: "Weather App",
         about: null,
         tags: ["ReactJs"],
@@ -29,7 +39,7 @@ export const ProjectData = [
         image: "https://github.com/FacundoZto/Weather-App/blob/main/src/components/styles/weather-app.png?raw=true"
     }, 
     {
-        id: 4,
+        id: 5,
         title: "GitHub Users",
         about: null,
         tags: ["ReactJs"],
