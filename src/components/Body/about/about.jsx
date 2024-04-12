af50b55c0c0aae9760067fa155b4ffd0523ee3c6
facundozto <facundozerbato@outlook.com>
2024-04-12 14:09:08 -0300

    Update styles css

--- src/components/Body/about/about.jsx
+++ src/components/Body/about/about.jsx
@@ -1,34 +1,37 @@
-import './about.css';
-import coding from '../../../assets/coding.png';
-import SocialContact from '../../common/social-contact.jsx';
-import TechSkills from '../../common/skills.jsx';
+import "./about.css";
+import coding from "../../../assets/coding.png";
+import SocialContact from "../../common/social-contact.jsx";
+import TechSkills from "../../common/skills.jsx";
 
 const About = () => {
-    return(
-        <div className='about'>
-            <div className='about-top'>
-                <div className='about-info'>
-                    {/* <span className='first-letter'>Hi there 👋</span> */}
-                    <p><span className='first-letter'>C</span>omencé en el mundo de la programación a través de Henry Bootcamp, el cual me permitió 
-                        desarrollarme como Full Stack Web Developer. Actualmente me oriento al Front End desarrollando
-                        proyectos para afianzar conceptos adquiridos y aprender nuevas tecnologías.<br/>
-                        Motivado para trabajar en equipo y en búsqueda de nuevos desafíos donde pueda aportar 
-                        valor y crecer profesionalmente. 
-                    </p>
-                    <SocialContact/>
-                    {/* <h4>Skills</h4>
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          {/* <span className='first-letter'>Hi there 👋</span> */}
+          <p>
+            <span className="first-letter">I </span>
+            started in the programming world through Henry Bootcamp, which
+            allowed me to develop as a Full Stack Web Developer. Currently, I
+            focus on Front End development, working on projects to reinforce
+            acquired concepts and learn new technologies.
+            <br /> Motivated to work in teams and seeking new challenges where I
+            can contribute value and grow professionally.
+          </p>
+          <SocialContact />
+          {/* <h4>Skills</h4>
                     <TechSkills/> */}
-                </div>
-                <div className='about-photo'>
-                    <img src={coding} className='picture' alt='Coding' />
-                </div>
-            </div>
-            <div className='about-bottom'>
-            <h4>Skills</h4>
-            <TechSkills/>
-            </div>
         </div>
-    )
-}
+        <div className="about-photo">
+          <img src={coding} className="picture" alt="Coding" />
+        </div>
+      </div>
+      <div className="about-bottom">
+        <h4>Skills</h4>
+        <TechSkills />
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Header/web/web.jsx
+++ src/components/Header/web/web.jsx
@@ -1,50 +1,56 @@
-import './web.css';
-import {motion} from 'framer-motion';
-import {Link} from 'react-scroll';
+import "./web.css";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 
 const Web = () => {
-    return(
-        <motion.div 
-        className='web'
-        transition={{
-            duration: 0.5,
-        }}
-        animate={{
-            x:-30,
-        }}
+  return (
+    <motion.div
+      className="web"
+      transition={{
+        duration: 0.5,
+      }}
+      animate={{
+        x: -30,
+      }}
+    >
+      <div className="web-option">
+        <Link
+          to="About"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={550}
+          className={"link"}
         >
-            <div className='web-option'>
-                <Link
-                to="About" 
-                spy={true} 
-                smooth={true} 
-                offset={-50} 
-                duration={550}
-                className={'link'}
-                >Sobre Mí</Link>
-            </div>
-            <div className='web-option'>
-                <Link
-                to="Projects" 
-                spy={true} 
-                smooth={true} 
-                offset={0} 
-                duration={550}
-                className={'link'}
-                >Proyectos</Link>
-            </div>
-            <div className='web-option'>
-                <Link
-                to="Contact" 
-                spy={true} 
-                smooth={true} 
-                offset={0} 
-                duration={550}
-                className={'link'}
-                >Contacto</Link>
-            </div>
-        </motion.div>
-    )
-}
+          About
+        </Link>
+      </div>
+      <div className="web-option">
+        <Link
+          to="Projects"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={550}
+          className={"link"}
+        >
+          Projects
+        </Link>
+      </div>
+      <div className="web-option">
+        <Link
+          to="Contact"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={550}
+          className={"link"}
+        >
+          Contact
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Web;
