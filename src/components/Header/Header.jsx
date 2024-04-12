88330c3275cb7356da87eb0e4e6a76039af20cb8
facundozto <facundozerbato@outlook.com>
2024-04-12 18:08:09 -0300

    add logo.ico

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,39 @@
-import './header.css';
-import Mobile from './mobile/mobile.jsx';
-import Web from './web/web.jsx';
-import { useState } from 'react';
-import {motion} from 'framer-motion';
+import "./header.css";
+import Mobile from "./mobile/mobile.jsx";
+import Web from "./web/web.jsx";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <div className='header'>
-            <motion.div 
-            className='title'
-            transition={{
-                duration: 0.5,
-            }}
-            animate={{
-                x:50,
-                
-            }}
-            >Facundo Zerbato</motion.div>
+  return (
+    <div className="header">
+      <motion.div
+        className="title"
+        transition={{
+          duration: 0.5,
+        }}
+        animate={{
+          x: 50,
+        }}
+      >
+        Front-end Developer
+      </motion.div>
 
-            <div className='menu'>
-                <div className='web-menu'><Web/></div>
-                <div className='mobile-menu'>
-                    <div onClick={() => setIsOpen(!isOpen)} >
-                        <span className="material-symbols-outlined icon-mobile">menu</span>
-                    </div>
-                    {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
-                </div>
-            </div>
+      <div className="menu">
+        <div className="web-menu">
+          <Web />
         </div>
-    )
-}
+        <div className="mobile-menu">
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <span className="material-symbols-outlined icon-mobile">menu</span>
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
