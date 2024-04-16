31c883022c210fed049f2525c972cfd0a84598af
facundozto <facundozerbato@outlook.com>
2024-04-16 14:49:24 -0300

    update url projects

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,50 +1,53 @@
-import img from '../../assets/github.png';
-import travel from '../../assets/travel.png';
+import img from "../../assets/github.png";
+import travel from "../../assets/travel.png";
 
 export const ProjectData = [
-    {
-        id: 1,
-        title: "Travel App",
-        about: "Single Page Application",
-        tags: ["ReactJs"],
-        demo: "https://travel-company.vercel.app/",
-        github: "https://github.com/FacundoZto/TravelProject",
-        image: travel
-    },
-    {
-        id: 2,
-        title: "Dogs App",
-        about: "Single Page Application",
-        tags: ["ReactJs", "Redux", "NodeJs"],
-        demo: "https://dogs-app-iota.vercel.app",
-        github: "https://github.com/FacundoZto/Proyecto-Dogs",
-        image: "https://raw.githubusercontent.com/FacundoZto/Proyecto-Dogs/main/Imagenes/Landing.png"
-    },
-    {
-        id: 3,
-        title: "React CRUD",
-        about: "Create, Read, Update, Delete",
-        tags: ["ReactJs", "Redux", "NodeJs"],
-        demo: null,
-        github: "https://github.com/FacundoZto/React-CRUD",
-        image: "https://github.com/FacundoZto/React-CRUD/blob/main/img/React-crud-home.png?raw=true"
-    },
-    {
-        id: 4,
-        title: "Weather App",
-        about: null,
-        tags: ["ReactJs"],
-        demo: null,
-        github: "https://github.com/FacundoZto/Weather-App",
-        image: "https://github.com/FacundoZto/Weather-App/blob/main/src/components/styles/weather-app.png?raw=true"
-    }, 
-    {
-        id: 5,
-        title: "GitHub Users",
-        about: null,
-        tags: ["ReactJs"],
-        demo: null,
-        github: "https://github.com/FacundoZto/Prueba-Tecnica_Front-end",
-        image: img
-    }
-]
+  {
+    id: 1,
+    title: "Travel App",
+    about: "Single Page Application",
+    tags: ["ReactJs"],
+    demo: "https://travel-company.vercel.app/",
+    github: "https://github.com/FacundoZto/TravelProject",
+    image: travel,
+  },
+  {
+    id: 2,
+    title: "Dogs App",
+    about: "Single Page Application",
+    tags: ["ReactJs", "Redux", "NodeJs"],
+    demo: "https://dogs-ruddy-delta.vercel.app/",
+    github: "https://github.com/FacundoZto/Proyecto-Dogs",
+    image:
+      "https://raw.githubusercontent.com/FacundoZto/Proyecto-Dogs/main/Imagenes/Landing.png",
+  },
+  {
+    id: 3,
+    title: "React CRUD",
+    about: "Create, Read, Update, Delete",
+    tags: ["ReactJs", "Redux", "NodeJs"],
+    demo: null,
+    github: "https://github.com/FacundoZto/React-CRUD",
+    image:
+      "https://github.com/FacundoZto/React-CRUD/blob/main/img/React-crud-home.png?raw=true",
+  },
+  {
+    id: 4,
+    title: "Weather App",
+    about: null,
+    tags: ["ReactJs"],
+    demo: null,
+    github: "https://github.com/FacundoZto/Weather-App",
+    image:
+      "https://github.com/FacundoZto/Weather-App/blob/main/src/components/styles/weather-app.png?raw=true",
+  },
+  {
+    id: 5,
+    title: "GitHub Users",
+    about: null,
+    tags: ["ReactJs"],
+    demo: null,
+    github: "https://github.com/FacundoZto/Prueba-Tecnica_Front-end",
+    image: img,
+  },
+];
