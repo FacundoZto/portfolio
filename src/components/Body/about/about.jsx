124f53bc54de00d3e1849836ec521ee7a8292e3e
facundozto <facundozerbato@outlook.com>
2024-11-07 14:47:14 -0300

    Update about

--- src/components/Body/about/about.jsx
+++ src/components/Body/about/about.jsx
@@ -11,11 +11,9 @@ const About = () => {
           <p>
             <span className="first-letter">I </span>
             started in the programming world through Henry Bootcamp, which
-            allowed me to develop as a Full Stack Web Developer. Currently, I
-            focus on Front End development, working on projects to reinforce
+            allowed me to develop as a Full Stack Developer. Currently, I am focus on projects to reinforce
             acquired concepts and learn new technologies.
-            <br /> Motivated to work in teams and seeking new challenges where I
-            can contribute value and grow professionally.
+            <br /> I am motivated to work in teams and seek new challenges where I can add value and grow professionally.
           </p>
         </div>
         <div className="about-photo">
