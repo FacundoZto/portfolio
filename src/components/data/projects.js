8944bf4fbd1b9c6c405f041898ca6915411f9214
facundozto <facundozerbato@outlook.com>
2025-04-11 12:29:50 -0300

    update data projects

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,20 +1,30 @@
-import img from "../../assets/github.png";
 import travel from "../../assets/travel.png";
 import movie from "../../assets/movie.png";
+import dhestudio from "../../assets/dhestudio.png";
+
 
 export const ProjectData = [
   {
-    id: 1,
-    title: "Movie App",
-    about: "Website about movies that allows the user to perform searches using data from 'TheMovieDataBase' API. Technologies used: Next.js, React, TypeScript, TailwindCSS. Deployed on Vercel.",
-    tags: ["Nextjs"],
+    id: 6,
+    title: "DHestudio",
+    about: "App web for an accounting firm using Vite + React, Bootstrap for UI design, and Formik + Yup for form validation.The backend was built with Node.js, Express, and Nodemailer, allowing emails to be sent via the contact form. The project was deployed in Render.",
+    tags: ["ReactJs", "Bootstrap", "Formik", "NodeJs", "Express", "Nodemailer"],
+    demo: "https://estudiodh.onrender.com/",
+    github: "https://github.com/FacundoZto/estudio-dh-app",
+    image: dhestudio,
+  },
+  {
+    id: 5,
+    title: "Movies",
+    about: "App web for movie browsing using Typescript, Next.js and Tailwind CSS, consuming data from the TMDb API. Users can view movie details such as synopsis, release date, and ratings with a responsive interface.",
+    tags: ["Nextjs", "Typescript", "TailwindCSS", "ReactJs"],
     demo: "https://movie-nextjs-mdb.vercel.app/",
     github: "https://github.com/FacundoZto/movie-app",
     image: movie,
   },
   {
-    id: 2,
-    title: "Travel App",
+    id: 4,
+    title: "Travels",
     about: "Responsive single page application project containing sections for About, Services, Products, Details, and Contact. Developed with React, TypeScript, CSS, and Framer Motion for animations. Deployed on Vercel.",
     tags: ["ReactJs"],
     demo: "https://travel-company.vercel.app/",
@@ -32,7 +42,17 @@ export const ProjectData = [
       "https://raw.githubusercontent.com/FacundoZto/Proyecto-Dogs/main/Imagenes/Landing.png",
   },
   {
-    id: 4,
+    id: 2,
+    title: "Weather App",
+    about: null,
+    tags: ["ReactJs"],
+    demo: null,
+    github: "https://github.com/FacundoZto/Weather-App",
+    image:
+    "https://github.com/FacundoZto/Weather-App/blob/main/src/components/styles/weather-app.png?raw=true",
+  },
+  {
+    id: 1,
     title: "React CRUD",
     about: "Create, Read, Update, Delete",
     tags: ["ReactJs", "Redux", "NodeJs"],
@@ -40,24 +60,5 @@ export const ProjectData = [
     github: "https://github.com/FacundoZto/React-CRUD",
     image:
       "https://github.com/FacundoZto/React-CRUD/blob/main/img/React-crud-home.png?raw=true",
-  },
-  {
-    id: 5,
-    title: "Weather App",
-    about: null,
-    tags: ["ReactJs"],
-    demo: null,
-    github: "https://github.com/FacundoZto/Weather-App",
-    image:
-      "https://github.com/FacundoZto/Weather-App/blob/main/src/components/styles/weather-app.png?raw=true",
-  },
-  {
-    id: 6,
-    title: "GitHub Users",
-    about: null,
-    tags: ["ReactJs"],
-    demo: null,
-    github: "https://github.com/FacundoZto/Prueba-Tecnica_Front-end",
-    image: img,
   },
 ];
